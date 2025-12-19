8ee4a64d479576a337f5596578598d45c991e0bb
givoxxs <toanyogame@gmail.com>
2025-12-19 22:38:51 +0700

    refactor(providers): dynamically import I18nProvider for client-side rendering

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { I18nProvider } from "@/components/providers/I18nProvider";
+// import { I18nProvider } from "@/components/providers/I18nProvider";
+import dynamic from "next/dynamic";
 import { ToastContainer } from "react-toastify";
 import Toast from "@/components/providers/Toast";
 import "react-toastify/dist/ReactToastify.css";
+
+const I18nProvider = dynamic(
+  () => import("@/components/providers/I18nProvider").then((mod) => mod.I18nProvider),
+  { ssr: false }
+);
 
 export default function Providers({
   children,
